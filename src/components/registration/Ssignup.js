47fa32c1c4f24e59b0506194b46e56d81abc2b7e
Andrew-darkweb <12yt11@mail.ru>
2025-11-01 10:16:15 +0300

    123

--- src/components/registration/Ssignup.js
+++ src/components/registration/Ssignup.js
@@ -10,4 +10,69 @@ export const Wrapper = styled.div`
 	height: 100%;
 	overflow-x: hidden;
 	overflow-y: scroll;
-	background-color: #EAEEF6;
+	background-color: #EAEEF6; `;
+export const Container-signup = styled.div`
+	display: block;
+ 	width: 100vw;
+  	min-height: 100vh;
+ 	margin: 0 auto; `;
+export const Modal = styled.div`
+  width: 100%;
+  height: 100%;
+  min-width: 320px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center; `;
+
+export const Modal-block = styled.div`
+  margin: 0 auto;
+  background-color: #FFFFFF;
+  max-width: 368px;
+  width: 100%;
+  padding: 50px 60px;
+  border-radius: 10px;
+  border: 0.7px solid #D4DBE5;
+  box-shadow: 0px 4px 67px -12px rgba(0, 0, 0, 0.13); `;
+
+export const Modal-ttl = styled.div`
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 30px;
+  letter-spacing: -0.6px;
+  margin-bottom: 20px; `;
+
+export const Modal-form-login = styled.form`
+	width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center; `;
+
+export const Modal-input = styled.input`
+  width: 100%;
+  min-width: 100%;
+  border-radius: 8px;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  outline: none;
+  padding: 10px 8px; `;
+
+export const Modal-btn-signup-ent = styled.button`
+  width: 100%;
+  height: 30px;
+  background-color: #565EEF;
+  border-radius: 4px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 21px;
+  font-weight: 500;
+  letter-spacing: -0.14px;
+  color: #FFFFFF; `;
